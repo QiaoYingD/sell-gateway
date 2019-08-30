ff7bc78772a605d0675e62f2cb84f39858e30e87
乔应东@qiayingdong.pn <qiaoyingdong.pn@rdunicloud.com>
2019-08-30 17:36:54 +0800

    添加微服务 网关

---
+++ src/main/java/com/imooc/sellgateway/SellGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.sellgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/sellgateway/SellGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.sellgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
